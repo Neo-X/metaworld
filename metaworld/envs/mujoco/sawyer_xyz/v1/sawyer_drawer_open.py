af82a1f9fc9faaf4a3301bcd0b5a84192d0fdcf6
Zhiwei-Z <zhiweizhang2012@gmail.com>
2021-01-14 14:08:45 -0800

    set render drawer-open-v1

--- metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_drawer_open.py
+++ metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_drawer_open.py
@@ -6,7 +6,7 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _asser
 
 
 class SawyerDrawerOpenEnv(SawyerXYZEnv):
-    def __init__(self):
+    def __init__(self, render_env=False):
 
         hand_low = (-0.5, 0.40, 0.05)
         hand_high = (0.5, 1, 0.5)
@@ -14,6 +14,8 @@ class SawyerDrawerOpenEnv(SawyerXYZEnv):
         obj_high = (0.1, 0.9, 0.04)
         goal_low = (-0.1, 0.5499, 0.04)
         goal_high = (0.1, 0.5501, 0.04)
+
+        self.render_env = render_env
 
         super().__init__(
             self.model_name,
@@ -38,6 +40,9 @@ class SawyerDrawerOpenEnv(SawyerXYZEnv):
         )
         self.goal_space = Box(np.array(goal_low), np.array(goal_high))
 
+    def set_render(self, render_env):
+        self.render_env = render_env
+
     @property
     def model_name(self):
         return get_asset_full_path('sawyer_xyz/sawyer_drawer.xml')
@@ -54,6 +59,9 @@ class SawyerDrawerOpenEnv(SawyerXYZEnv):
             'pickRew': None,
             'success': float(pullDist <= 0.08)
         }
+
+        if self.render_env:
+            info['rendering'] = self.render(mode=self.render_env)
 
         return ob, reward, False, info
 
