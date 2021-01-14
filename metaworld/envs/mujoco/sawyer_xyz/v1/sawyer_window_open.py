1867c35bbb6b18649d38ff698eaccede16e7ebc4
Zhiwei-Z <zhiweizhang2012@gmail.com>
2021-01-14 14:12:27 -0800

    set render window-open-v1

--- metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_window_open.py
+++ metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_window_open.py
@@ -7,13 +7,15 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _asser
 
 class SawyerWindowOpenEnv(SawyerXYZEnv):
 
-    def __init__(self):
+    def __init__(self, render_env=False):
 
         liftThresh = 0.02
         hand_low = (-0.5, 0.40, 0.05)
         hand_high = (0.5, 1, 0.5)
         obj_low = (-0.1, 0.7, 0.16)
         obj_high = (0.1, 0.9, 0.16)
+
+        self.render_env = render_env
 
         super().__init__(
             self.model_name,
@@ -43,6 +45,9 @@ class SawyerWindowOpenEnv(SawyerXYZEnv):
         )
         self.goal_space = Box(np.array(goal_low), np.array(goal_high))
 
+    def set_render(self, render_env):
+        self.render_env = render_env
+
     @property
     def model_name(self):
         return get_asset_full_path('sawyer_xyz/sawyer_window_horizontal.xml')
@@ -60,6 +65,9 @@ class SawyerWindowOpenEnv(SawyerXYZEnv):
             'pickRew': pickrew,
             'success': float(pullDist <= 0.05)
         }
+
+        if self.render_env:
+            info['rendering'] = self.render(mode=self.render_env)
 
         return ob, reward, False, info
 
