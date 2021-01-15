c4820c53ac6dfdfee2935980f166822c215d733c
Zhiwei-Z <zhiweizhang2012@gmail.com>
2021-01-14 16:36:57 -0800

    set render button-press-topdown-wall-v1

--- metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_button_press_topdown_wall.py
+++ metaworld/envs/mujoco/sawyer_xyz/v1/sawyer_button_press_topdown_wall.py
@@ -7,12 +7,14 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _asser
 
 class SawyerButtonPressTopdownWallEnv(SawyerXYZEnv):
 
-    def __init__(self):
+    def __init__(self, render_env=False):
 
         hand_low = (-0.5, 0.40, 0.05)
         hand_high = (0.5, 1, 0.5)
         obj_low = (-0.05, 0.8, 0.05)
         obj_high = (0.05, 0.9, 0.05)
+
+        self.render_env = render_env
 
         super().__init__(
             self.model_name,
@@ -39,6 +41,9 @@ class SawyerButtonPressTopdownWallEnv(SawyerXYZEnv):
         )
         self.goal_space = Box(np.array(goal_low), np.array(goal_high))
 
+    def set_render(self, render_env):
+        self.render_env = render_env
+
     @property
     def model_name(self):
         return get_asset_full_path('sawyer_xyz/sawyer_button_press_topdown_wall.xml')
@@ -55,6 +60,9 @@ class SawyerButtonPressTopdownWallEnv(SawyerXYZEnv):
             'pickRew': None,
             'success': float(pressDist <= 0.02)
         }
+
+        if self.render_env:
+            info['rendering'] = self.render(mode=self.render_env)
 
         return ob, reward, False, info
 
